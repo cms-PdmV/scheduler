ac00ae3e8165f93f30fc9dc15ec38dfcd1482cb3
Stepan Balcar <bc.stepan.balcar@gmail.com>
2013-08-23 13:24:15 +0200

    Python configuration file with constants

---
+++ Config.py
@@ -0,0 +1,30 @@
+###############################################################################
+###                       CHERRYPY CONFIGURATION                            ###
+###############################################################################
+
+SERVER_NAME = 'XXXXX VALUE XXXXX';
+
+
+###############################################################################
+###                       COUCHDB CONFIGURATION                             ###
+###############################################################################
+
+SERVERDB_NAME = 'XXXXX VALUE XXXXX';
+SERVERDB_PORT = 'XXXXX VALUE XXXXX';
+DATABASE_NAME = 'XXXXX VALUE XXXXX';
+
+LINK_SORTED_DEADLINE_TIME_BLOCKS = '_design/XXXXX VALUE XXXXX';
+LINK_SORTED_PRIORITY_TIME_BLOCKS = '_design/XXXXX VALUE XXXXX';
+
+LINK_ID_OF_PLANDED_TIME_BLOCKS = '_design/XXXXX VALUE XXXXX';
+LINK_PLANDED_TIME_BLOCKS = '_design/XXXXX VALUE XXXXX';
+
+
+
+###############################################################################
+###                       SCHEDULER CONFIGURATION                           ###
+###############################################################################
+
+EVENT_MAX = 1000;
+
+SCHEDULER_OUTPUT_FILEJS = './javaScript/d3jsTimeTableData.js';
